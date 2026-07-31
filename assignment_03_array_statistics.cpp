97cb69063a11863badb9999ecaae4931d546c677
Titi-1-mufi <mufidamaltitinat@gmail.com>
2026-07-31 11:54:13 +0000

    asignmaent 3 completed

--- assignment_03_array_statistics.cpp
+++ assignment_03_array_statistics.cpp
@@ -41,4 +41,47 @@
 
 #include <iostream>
 using namespace std;
-
+int calculatesum(int numbers {}, int n){
+    intsum =0;
+    for (int m = 0; m < n; m++){
+        sum += numbers[m];
+    }
+    return sum;
+}
+double calculateaverage(int numbers[], int n){
+    int sum = calculatesum(numbers, n);
+    return (double)sum / n;
+}
+int findmaximum(intnumbers[], int n){
+    int maximum = numbers[0];
+    for ( int m = 1; m < n; m++){
+        if (numbers[m] > maximum){
+            maximum = numbers[m];
+        }
+    }
+    return maximum;
+}
+intmain(){
+    int n;
+cout << "how many numbers?";
+cin >> n;
+if (n <= 0){
+    cout << "error: number of values must be positve."<<end1;
+    return 0;
+}
+int numbers[n];
+for (int m = 0; m < n; m++){
+    cout << "enter number" << m + 1 << ":";
+cin >> numbers[m];
+}
+cout <, "\nresults" end1;
+cout << "sum:" <<
+calculatesum(numbers, n) << end1;
+cout << "average:" << 
+calculateaverage(numbers, n) << end1;
+cout << "maximum:" <<
+findmaximum(numbers, n) << end1;
+cout << " minimum:" <<
+findminimum(numbers, n) << end1;
+return 0;
+}
